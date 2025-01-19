7af50612dbc205bc0b849ff10fb97799f44635b3
noviandsh <noviandsh@gmail.com>
2025-01-19 18:32:09 +0700

    First commit

---
+++ iFocus/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SettingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PCNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegisterPCToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(713, 415)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Firebrick
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.PaleTurquoise
+        Me.Label1.Location = New System.Drawing.Point(-1, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(801, 153)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "FOKUS"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SettingToolStripMenuItem
+        '
+        Me.SettingToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PCNameToolStripMenuItem, Me.RegisterPCToolStripMenuItem})
+        Me.SettingToolStripMenuItem.Name = "SettingToolStripMenuItem"
+        Me.SettingToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.SettingToolStripMenuItem.Text = "Setting"
+        '
+        'PCNameToolStripMenuItem
+        '
+        Me.PCNameToolStripMenuItem.Enabled = False
+        Me.PCNameToolStripMenuItem.Name = "PCNameToolStripMenuItem"
+        Me.PCNameToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PCNameToolStripMenuItem.Text = "PC Unregistered"
+        '
+        'RegisterPCToolStripMenuItem
+        '
+        Me.RegisterPCToolStripMenuItem.Name = "RegisterPCToolStripMenuItem"
+        Me.RegisterPCToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RegisterPCToolStripMenuItem.Text = "Register PC"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "iFocus"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SettingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PCNameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RegisterPCToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ iFocus/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Timers
+Public Class Form1
+    Dim WithEvents Timer As New Timer(5000)
+    Dim blockScreen = 0
+    Dim PCname = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\iFocus", "Name", Nothing)
+    Sub tampilData()
+        PCname = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\iFocus", "Name", Nothing)
+
+        Konek()
+
+        da = New MySqlDataAdapter("SELECT * FROM account", Koneksi)
+        dt = New DataTable
+        da.Fill(dt)
+        'DGV.Rows.Clear()
+        'For i = 0 To dt.Rows.Count - 1
+        '    DGV.Rows.Add(dt.Rows(i).Item(0))
+        '    DGV.Rows(i).Cells(1).Value = dt.Rows(i).Item(1)
+        '    DGV.Rows(i).Cells(2).Value = dt.Rows(i).Item(2)
+        'Next
+        'MsgBox(dt.Rows(0).Item(0))
+        'Label1.Text = dt.Rows(0).Item(1)
+        blockScreen = dt.Rows(0).Item(1)
+        System.Console.WriteLine(blockScreen)
+        System.Console.WriteLine(PCname)
+        If Me.InvokeRequired Then
+            Me.BeginInvoke(New Action(
+                           Sub()
+                               If blockScreen = 0 Then
+                                   Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedDialog
+                                   Me.WindowState = FormWindowState.Normal
+                                   Me.TopMost = False
+                               ElseIf blockScreen = 1 Then
+                                   Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+                                   Me.WindowState = FormWindowState.Maximized
+                                   Me.TopMost = True
+                                   MenuStrip1.Visible = False
+                               End If
+
+                           End Sub))
+        End If
+        Diskonek()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer.Start()
+        Me.TransparencyKey = Color.LightBlue
+        Me.BackColor = Color.LightBlue
+
+        PCNameToolStripMenuItem.Text = PCname
+        If PCname IsNot Nothing Then
+            RegisterPCToolStripMenuItem.Text = "Unregister PC"
+        End If
+    End Sub
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If (e.CloseReason = CloseReason.UserClosing) Then
+            If blockScreen = 1 Then
+                e.Cancel = True
+            End If
+        End If
+    End Sub
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.X Then
+            Application.Exit()
+            MsgBox("wkwkwk")
+        End If
+    End Sub
+    Private Sub Timer_Elapsed(sender As Object, e As ElapsedEventArgs) Handles Timer.Elapsed
+        tampilData()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Registry.CurrentUser.CreateSubKey("Software\iFocus")
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\iFocus", "name", "PC01")
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.Icon = SystemIcons.Application
+            'NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            'NotifyIcon1.BalloonTipTitle = "iFocus"
+            'NotifyIcon1.BalloonTipText = "iFocus"
+            'NotifyIcon1.ShowBalloonTip(50000)
+            ShowInTaskbar = False
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(sender As Object, e As EventArgs) Handles NotifyIcon1.DoubleClick
+        ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub RegisterPCToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RegisterPCToolStripMenuItem.Click
+
+    End Sub
+End Class
